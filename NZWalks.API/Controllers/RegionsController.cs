3e486df1a4a398b495503c10131445a0fa71f412
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-03-21 15:52:31 +0500

    update dto and delete using id

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -106,7 +106,53 @@ namespace NZWalks.API.Controllers
             return CreatedAtAction(nameof(GetById),new {id = regionDto.Id},regionDto);
         }
 
+        /// <summary>
+        /// PUT:  https://localhost:portnumber/api/regions
+        /// </summary>
+        /// <returns>
+        /// Update existing region
+        /// </returns>
+        [HttpPut]
+        public IActionResult Update(Guid id,UpdateRegionDto updateRegionDto)
+        {
+            var regionDomainModel=dbContext.Regions.Find(id);
+            //check exist or not
+            if(regionDomainModel == null) { return NotFound();}
+            //update values
+            regionDomainModel.Code = updateRegionDto.Code;
+            regionDomainModel.Name = updateRegionDto.Name;
+            regionDomainModel.ImageUrl = updateRegionDto.ImageUrl;
+            dbContext.SaveChanges();
+            //map domain to dto
+            var updatedRegionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Name = regionDomainModel.Name,
+                Code = regionDomainModel.Code,
+                ImageUrl = regionDomainModel.ImageUrl,
+            };
+            return Ok(updatedRegionDto); 
+        }
 
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult Delete([FromRoute]Guid id)
+        {
+            var redionDomainModel = dbContext.Regions.Find(id);
+            if (redionDomainModel == null) { return NotFound(); }
+            //delete
+            dbContext.Regions.Remove(redionDomainModel);
+            dbContext.SaveChanges();
+            //map domain to dto
+            var regionDto = new RegionDto
+            {
+                Id = redionDomainModel.Id,
+                Name = redionDomainModel.Name,
+                Code = redionDomainModel.Code,
+                ImageUrl = redionDomainModel.ImageUrl,
+            };
+            return Ok(regionDto);
+        }
 
     }
 }
---
+++ NZWalks.API/Models/Dto/UpdateRegionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NZWalks.API.Models.Dto
+{
+    public class UpdateRegionDto
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
